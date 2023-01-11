f94cfb7459f324f07e00d1191dd9c29f8238378e
Vinaya R <vinayar@FVHVGX7WJ1WK.local>
2023-01-11 15:16:31 +0530

    deployement

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT=process.env.PORT || 8000
 const oauth2Client = new google.auth.OAuth2(
     "794602203764-l4p5vpnlqrt9pmsdhocbbqfvjlgrbfmc.apps.googleusercontent.com",
     "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",
-    'http://127.0.0.1:8000/callback'
+    'https://indus-373613.el.r.appspot.com/callback'
   );
 
 function getGoogleAuthURL() {
@@ -27,15 +27,19 @@ function getGoogleAuthURL() {
 
 
  app.get("/authorize", (req, res) => {
-    return res.redirect(getGoogleAuthURL());
+    res.redirect(getGoogleAuthURL());
   });
   
 
 app.get("/callback",async(req,res)=>{
 
+   
+    if(req.query.error) return res.redirect(getGoogleAuthURL());
+
     const code = req.query.code
+    console.log(code)
     const tokens  = await oauth2Client.getToken(code)
-    return res.send(tokens)
+    res.send(tokens)
 });
 
 
