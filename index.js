b00dd7c47d8a61af12b3209ca049d737aca2c557
Vinaya R <vinayar@FVHVGX7WJ1WK.local>
2023-01-12 11:36:48 +0530

    Checking if user has provided all the scope

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const {google} = require('googleapis');
 const express = require('express');
 const {OAuth2Client} = require('google-auth-library');
-
+const axios = require('axios')
 
 const app = express();
 const PORT=process.env.PORT || 8000
 
-  const oAuth2Client = new OAuth2Client(
+const oAuth2Client = new OAuth2Client(
     "794602203764-l4p5vpnlqrt9pmsdhocbbqfvjlgrbfmc.apps.googleusercontent.com",
    "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",
    'http://127.0.0.1:8000/callback'
 );
 
-  function getAuthenticatedClient() {
+const scopes = [
+
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/gmail.readonly',
+  'openid'
+    
+];
+
+function getAuthenticatedClient() {
+
     return new Promise((resolve, reject) => {
-
-      const scopes = [
-        'https://www.googleapis.com/auth/gmail.readonly',
-        'https://www.googleapis.com/auth/userinfo.profile'
-
-    ];
-
       const authorizeUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: scopes
@@ -28,7 +31,6 @@ const PORT=process.env.PORT || 8000
         
          resolve(authorizeUrl)
     })}
-
 
  app.get("/authorize", (req, res) => {
 
@@ -44,7 +46,21 @@ app.get("/callback",async(req,res)=>{
 
     const code = req.query.code
     const tokens = await oAuth2Client.getToken(code);
-    res.status(200).send("Thankyou for the permission....You can return back to the app");
+    //axios({method:'GET',url:`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${tokens.tokens.access_token}`}).then((response)=>{
+        
+        //let array = response.data.scope.split(' ');
+        //console.log(tokens.tokens.scope)
+        /*if(JSON.stringify(array2) === JSON.stringify(scopes))
+        {
+            return res.send(tokens)
+        }})*/
+        let array2=tokens.tokens.scope.split(' ')
+        if(array2.length === scopes.length && array2.every(elem => scopes.includes(elem)))
+           return res.send(tokens)
+        else 
+           return getAuthenticatedClient().then((authorizeUrl)=>res.redirect(authorizeUrl))
+          
+    
 });
 
 
